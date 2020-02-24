a8983825aadb1ae7ae0c934f05ea8b3cba19b213
RichStone <richard.had@hotmail.de>
2020-02-24 21:22:41 +0100

    add signup/login links to navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Routes from "./Routes";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 
 function App(props) {
@@ -10,11 +11,20 @@ function App(props) {
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
-            <Link to="/">NumisApp </Link>
-            <Link to="/asdf">Second</Link>
+            <Link to="/">Scratch</Link>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <LinkContainer to="/signup">
+              <NavItem>Signup</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <NavItem>Login</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
