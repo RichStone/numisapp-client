723a7a1dc73120a483cc429731c9182112319001
RichStone <richard.had@hotmail.de>
2020-02-24 21:38:44 +0100

    set up aws amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "numisapp-uploads"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://9r3jffs970.execute-api.eu-central-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_UMUf9OOUN",
+    APP_CLIENT_ID: "19tsohu1u8du4grrc5obp7ijc4",
+    IDENTITY_POOL_ID: "eu-central-1:ce100f6d-63ce-41e0-ac8f-1380c93e527f"
+  }
+};
