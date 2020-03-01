ca7e879dabeb5b7dd51e7a2b639513860ff477e6
RichStone <richard.had@hotmail.de>
2020-03-01 12:54:05 +0100

    add notes list

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { API } from "aws-amplify";
 import "./Home.css";
 
-export default function Home() {
-  return (
-    <div className="Home">
+export default function Home(props) {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function onLoad() {
+      if (!props.isAuthenticated) {
+        return;
+      }
+
+      try {
+        const products = await loadProducts();
+        setProducts(products);
+      } catch (e) {
+        alert(e);
+      }
+
+      setIsLoading(false);
+    }
+
+    onLoad();
+  }, [props.isAuthenticated]);
+
+  function loadProducts() {
+    return API.get("products", "/products");
+  }
+
+  function renderProductsList(products) {
+    return [{}].concat(products).map((product, i) =>
+      i !== 0 ? (
+        <LinkContainer
+          key={product.ProductId}
+          to={`/products/${product.ProductId}`}
+        >
+          <ListGroupItem header={product.Category}>
+            {"Created: " + new Date(product.CreatedAt).toLocaleString()}
+          </ListGroupItem>
+        </LinkContainer>
+      ) : (
+        <LinkContainer key="new" to="/products/new">
+          <ListGroupItem>
+            <h4>
+              <b>{"\uFF0B"}</b> Create a new product
+            </h4>
+          </ListGroupItem>
+        </LinkContainer>
+      )
+    );
+  }
+
+  function renderLander() {
+    return (
       <div className="lander">
         <h1>Numis App</h1>
-        <p>Numismat's Nr. 1 Digital Friend</p>
+        <p>Numismat's Number 1 digital friend.</p>
       </div>
+    );
+  }
+
+  function renderProducts() {
+    return (
+      <div className="products">
+        <PageHeader>Your Products</PageHeader>
+        <ListGroup>{!isLoading && renderProductsList(products)}</ListGroup>
+      </div>
+    );
+  }
+
+  return (
+    <div className="Home">
+      {props.isAuthenticated ? renderProducts() : renderLander()}
     </div>
   );
 }
