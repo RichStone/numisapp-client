77838c8395ee582df93035ec92db23b5c209b9b6
RichStone <richard.had@hotmail.de>
2020-03-08 16:01:10 +0100

    add stage configs

--- src/config.js
+++ src/config.js
@@ -1,18 +1,44 @@
-export default {
+const dev = {
+  STRIPE_KEY: "pk_test_y9HGkQIextdPxPdKJ8y0cvVd00YpseB17I",
   s3: {
     REGION: "eu-central-1",
-    BUCKET: "numisapp-uploads"
+    BUCKET: "numify-dev-attachmentsbucket-eaoukmgvhz47"
   },
   apiGateway: {
     REGION: "eu-central-1",
-    URL: "https://9r3jffs970.execute-api.eu-central-1.amazonaws.com/dev"
+    URL: "https://api.numify.co/dev"
   },
   cognito: {
     REGION: "eu-central-1",
-    USER_POOL_ID: "eu-central-1_UMUf9OOUN",
-    APP_CLIENT_ID: "19tsohu1u8du4grrc5obp7ijc4",
-    IDENTITY_POOL_ID: "eu-central-1:ce100f6d-63ce-41e0-ac8f-1380c93e527f"
-  },
+    USER_POOL_ID: "eu-central-1_ZlifrrWdR",
+    APP_CLIENT_ID: "m0a89nksg6ejch59194l710tg",
+    IDENTITY_POOL_ID: "eu-central-1:43b979b3-8871-45fc-8a59-8b9fc751b41f"
+  }
+};
+
+const prod = {
   STRIPE_KEY: "pk_test_y9HGkQIextdPxPdKJ8y0cvVd00YpseB17I",
-  MAX_ATTACHMENT_SIZE: 5000000
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "numify-prod-attachmentsbucket-1fqupxotx72lf"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://api.numify.co/prod"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_c8D3g4hxv",
+    APP_CLIENT_ID: "7bsormil1soo7e1mpgkrpk4c64",
+    IDENTITY_POOL_ID: "eu-central-1:58e6270f-f45b-4fe0-a876-a6546350d6c5"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === "prod" ? prod : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
