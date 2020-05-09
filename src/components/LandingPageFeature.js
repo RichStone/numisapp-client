87664cb4129f5d31da2447acd588ecafa6c25f44
RichStone <richard.had@hotmail.de>
2020-05-09 15:57:01 +0200

    add feature explanations

--- src/components/LandingPageFeature.js
+++ src/components/LandingPageFeature.js
@@ -1,3 +1,18 @@
 import React from "react";
+import { Col, Row, Image } from "react-bootstrap";
+import "./LandingPageFeature.css";
 
-export default function LandingPageFeature(props) {}
+export default function LandingPageFeature(props) {
+  return (
+    <div className="LandingPageFeature">
+      <Row>
+        <Col md={4}>
+          <Image src={`/images/${props.imgName}`} rounded />
+        </Col>
+        <Col md={8}>
+          <p>{props.description}</p>
+        </Col>
+      </Row>
+    </div>
+  );
+}
--- src/containers/Lander.js
+++ src/containers/Lander.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Image, Jumbotron, Button } from "react-bootstrap";
 import PricingTableComponent from "../components/PricingTable";
+import LandingPageFeature from "../components/LandingPageFeature";
 import "./Lander.css";
 
 export default function Lander(props) {
@@ -8,6 +9,28 @@ export default function Lander(props) {
     fontWeight: "bold",
     fontSize: 22,
   };
+
+  const maShopsFeatureDesc = `
+    MA-Shops is world's biggest platform for numismatic articles, but
+    the integration is not always optimal. We are here to fix this!
+  `;
+
+  const excelFeatureDesc = `
+    Are you tired of doing the same work for every platform? 
+    With numify you add your coins once, coins are sent to e-commerce platforms automatically.
+    Now you can focus on your core business.
+  `;
+
+  const customShopFeatureDesc = `
+    We integrate with your own custom shop solution, just let us know your e-commerce platform.
+    Don't have your own shop yet?
+    We help you get started with the perfect solution for your needs!
+  `;
+
+  const ebayFeatureDesc = `
+    We work on further integrations with eBay and catawiki. 
+    Please, let us know about your preferences!
+  `;
 
   return (
     <div className="lander">
@@ -18,7 +41,7 @@ export default function Lander(props) {
           <span style={inlineBoldStyle}>sell more coins easier!</span> List your
           coins in the numify app and they will be uploaded automatically to
           eBay, ma-shops, catawiki, and your own shop system. You have full
-          control.
+          control about your new inventory in one place.
         </p>
         <p>
           <a
@@ -30,7 +53,27 @@ export default function Lander(props) {
           </a>
         </p>
       </Jumbotron>
-      ;
+
+      <LandingPageFeature
+        imgName="ma-shops-logo.png"
+        description={maShopsFeatureDesc}
+      ></LandingPageFeature>
+
+      <LandingPageFeature
+        imgName="excel-logo.png"
+        description={excelFeatureDesc}
+      ></LandingPageFeature>
+
+      <LandingPageFeature
+        imgName="bigcommerce-logo.png"
+        description={customShopFeatureDesc}
+      ></LandingPageFeature>
+
+      <LandingPageFeature
+        imgName="ebay-logo.png"
+        description={ebayFeatureDesc}
+      ></LandingPageFeature>
+
       <div>
         <PricingTableComponent />
       </div>
